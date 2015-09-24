5b6bb8a1a2fd307d503ee9641e7cc69f672de5b1
Calvin Aditya Jonathan <deus.caj@gmail.com>
2015-09-24 14:21:16 +0700

    Update transmitter.c

--- transmitter.c
+++ transmitter.c
@@ -1,4 +1,3 @@
-#include <errno.h>
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
@@ -18,60 +17,74 @@ const char* portname;
 struct addrinfo hints;
 struct addrinfo* res = 0;
 int fd;
+Boolean xonoff = true;
 
 pthread_t xonoff_thread;
 
+//Prosedur untuk thread anak
 void *listen_xonoff(void *dummy)
 {
 	int recvlen;
 	char bridge[128];
 	while (true) {
 		recvlen = recvfrom(fd,bridge,sizeof(bridge),0,res->ai_addr,&res->ai_addrlen);
-		if (bridge[0] == XOFF)
-			printf("XOFF diterima.\n");
+		if (recvlen > 0) {
+			if (bridge[0] == XOFF) {
+				printf("XOFF diterima.\n");
+				xonoff = false;
+			}
+			else if (bridge[0] == XON) {
+				printf("XON diterima.\n");
+				xonoff = true;
+			}
+		}
 	}
 }
 
-int main()
+int main(int argc, char *argv[])
 {
 	//Inisialisasi
-	hostname="127.0.0.1";
-	portname="2121";
-	int msglen = 13;
-	char content[13] = "HelloaWorld! ";
+	hostname=argv[1];
+	portname=argv[2];
+	FILE *input = fopen(argv[3], "r");
 	char buf[128];
-	int i;
-
 	memset(&hints,0,sizeof(hints));
 	hints.ai_family=AF_UNSPEC;
 	hints.ai_socktype=SOCK_DGRAM;
 	hints.ai_protocol=0;
 	hints.ai_flags=AI_ADDRCONFIG;
 	int err=getaddrinfo(hostname,portname,&hints,&res);
-	if (err!=0) {
-		//die("failed to resolve remote socket address (err=%d)",err);
-	}
 
 	//Buat socket
 	printf("Membuat socket untuk %s:%s...\n", hostname, portname);
 	fd=socket(res->ai_family,res->ai_socktype,res->ai_protocol);
-	if (fd==-1) {
-		//die("%s",strerror(errno));
-	}
 	
+	//Buat thread anak
 	pthread_create(&xonoff_thread, NULL, listen_xonoff, NULL);
 	
-	//Kirim message
-	for(i = 0;i < msglen;i++) {
-		buf[0] = content[i];
-		printf("Mengirim byte ke-%d: '%c'\n", i+1, buf[0]);
-		if (sendto(fd,buf,sizeof(buf),0,res->ai_addr,res->ai_addrlen)==-1) {
-			//die("%s",strerror(errno));
+	//Kirim message & looping file input
+	int i = 0;
+	char c = fgetc(input);
+	while (c != EOF) {
+		if (c == '\n')
+			c = fgetc(input);
+		else if (xonoff == false) {
+			printf("Menunggu XON...\n");
+			sleep(1);
 		}
-		sleep(1);
+		else if (xonoff == true) {
+			buf[0] = c;
+			printf("Mengirim byte ke-%d: '%c'\n", i+1, buf[0]);
+			if (sendto(fd,buf,sizeof(buf),0,res->ai_addr,res->ai_addrlen) == -1)
+				return -1;
+			i++;
+			c = fgetc(input);
+			sleep(1);
+		}
 	}
-
-	close(fd);
 	
+	fclose(input);
+	close(fd);
+		
 	return 0;
 }
